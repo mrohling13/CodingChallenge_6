df2fc007448243ae51d3aecdd1a085459532a659
mrohling13 <malachi.rohling@yahoo.com>
2025-02-08 22:30:34 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -18,3 +18,29 @@ const calculateSalesTax = function (amount, taxRate) {
 // Test Data for Task 2 
 console.log(calculateSalesTax(100, 0.07)); // Sales Tax: $7
 console.log(calculateSalesTax(500, 0.1)); // Sales Tax: $50
+
+// Task 3: Arrow Function
+const calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage;
+    switch (performanceRating) {
+        case "Excellent":
+        bonusPercentage = 0.2;
+            break;
+        case "Good":
+            bonusPercentage = 0.1;
+            break;
+        case "Average":
+            bonusPercentage = 0.05;
+            break;
+        default : 
+            bonusPercentage = 0;
+
+    }
+    const bonus = salary * bonusPercentage;
+    return `Bonus: $${bonus}`;
+};
+
+// Test Data for Task 3
+console.log(calculateBonus(5000, "Excellent")); // Bonus: $1000
+console.log(calculateBonus(7000, "Good")); // Bonus: $700
+
